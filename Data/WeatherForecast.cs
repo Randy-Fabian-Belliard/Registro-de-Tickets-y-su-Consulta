4796457cb982131be74595768e599db00fdc3a11
Randy <randyfabianbelliard191407@gmail.com>
2023-09-20 19:16:17 -0400

    Creando proyecto

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Registro_de_tickes.Data;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
