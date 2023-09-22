f4838abd4dfd706122182a3c81066c1210d4be90
Randy <randyfabianbelliard191407@gmail.com>
2023-09-22 19:51:00 -0400

    Creando modelo tickets

---
+++ Modelos/Tickets.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Tickets
+{
+
+    [Key]
+
+    public int TicketId {get; set;}
+
+    public DateTime Fecha {get; set;}
+    
+
+
+
+
+}
