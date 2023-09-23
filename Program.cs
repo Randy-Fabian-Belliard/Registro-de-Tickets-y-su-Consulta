089ea4e57fffdcb5cd79ca05b417bd079526d412
Randy <randyfabianbelliard191407@gmail.com>
2023-09-22 20:24:15 -0400

    Inyectando contexto

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Registro_de_tickes.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Leer la ConnectionStrings llamada "Constr" que puse en el appsettings.json
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+//Inyectar el contexto para que este disponible
+//En los  constructores donde lo solicitemos
+builder.Services.AddDbContext<Contexto>(options =>
+        options.UseSqlite(ConStr)
+);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
