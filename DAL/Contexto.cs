113f765d168a0f6d762104891eec04f3b897ad08
Randy <randyfabianbelliard191407@gmail.com>
2023-09-23 19:00:24 -0400

    Agragando Dbset de clientes al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -4,6 +4,7 @@ public class Contexto: DbContext
 {
 
     public DbSet<Tickets> Tickets {get; set;}
+    public DbSet<Clientes> Clientes { get; set; }
 
     public Contexto(DbContextOptions <Contexto> options): base (options)
     {
