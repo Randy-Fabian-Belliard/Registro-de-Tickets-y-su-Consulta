597bdd479e844c15670fc9d41f38281118539451
Randy <randyfabianbelliard191407@gmail.com>
2023-09-24 09:13:18 -0400

    Terminando moldelo de Tickets

--- Modelos/Tickets.cs
+++ Modelos/Tickets.cs
@@ -6,11 +6,18 @@ public class Tickets
     [Key]
 
     public int TicketId {get; set;}
-
+    [Required(ErrorMessage = "Este campo es nesesario")]
     public DateTime Fecha {get; set;}
-    
-
-
-
-
+    [Required(ErrorMessage = "Este campo es necesario")]
+    public int ClienteId { get; set; }
+    [Required(ErrorMessage = "Este campo es necesario")]
+    public int SistemaId { get; set; }
+    [Required(ErrorMessage = "Este campo necesario")]
+    public int PrioridadId { get; set; } 
+    [Required(ErrorMessage = "Este campo es necesario")]
+    public string? SolicitadoPor { get; set; }
+    [Required(ErrorMessage = "Este campo es necesario")]
+    public string? Asunto { get; set; }
+    [Required(ErrorMessage = "Este campo es necesario")]
+    public string? Descripcion { get; set; }
 }
