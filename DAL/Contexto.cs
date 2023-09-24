98dd1abaee4cf39b5eb0bc904ae9783d9009cab8
Randy <randyfabianbelliard191407@gmail.com>
2023-09-24 09:54:21 -0400

    Agreganso Sisttemas al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,6 +5,7 @@ public class Contexto: DbContext
 
     public DbSet<Tickets> Tickets {get; set;}
     public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Sistemas> Sistemas { get; set; }
 
     public Contexto(DbContextOptions <Contexto> options): base (options)
     {
