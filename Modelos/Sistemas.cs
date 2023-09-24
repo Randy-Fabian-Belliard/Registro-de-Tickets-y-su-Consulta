894c63ba00e5e91004be22de1732a7e607276ccb
Randy <randyfabianbelliard191407@gmail.com>
2023-09-24 09:52:10 -0400

    Agragando modelo de sistemas

---
+++ Modelos/Sistemas.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Sistemas
+{
+    [Key]
+    public int SistemaId { get; set; }
+
+    [Required (ErrorMessage ="El nombre del sistema es obligatorio")]
+    public String? Nombre { get; set; }
+}
