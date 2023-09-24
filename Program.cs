41a9feceeb3ef882d04ab35a9e9550cdd7a37e56
Randy <randyfabianbelliard191407@gmail.com>
2023-09-24 15:31:27 -0400

    Inyectando la BLL de prioridades

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<TicketsBLL>();
 builder.Services.AddScoped<ClientesBLL>();
 builder.Services.AddScoped<SistemasBLL>();
+builder.Services.AddScoped<PrioridadesBLL>();
 
 var app = builder.Build();
 
